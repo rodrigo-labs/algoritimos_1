b23376945dd520efbf3473d30b654e70c2e7aeef
Rodrigo Mendes Vieira <rodrigomv.dev@gmail.com>
2016-06-21 17:03:47 -0300

    exercicios da aula 2

---
+++ aula2/Exercicio2.java
@@ -0,0 +1,9 @@
+import javax.swing.JOptionPane;
+
+public class Exercicio2 {
+    
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null,"Rodrigo Mendes vieira \n606592 \n48 96628747");
+    }
+}
---
+++ aula2/Exercicio3.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class Exercicio3 {
+    
+    public static void main(String[] args) {
+	
+        JOptionPane.showMessageDialog(null,"UNISUL\nUniversidade do Sul de Santa Catarina"
+					  +"\nAluno: Rodrigo Mendes vieira" 
+					  +"\nCurso: Sistemas de informação"
+					  +"\nMatricula: 606592 ");
+    }
+}
